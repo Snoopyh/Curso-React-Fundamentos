92101b0d5cf40b3197f24c0d1e1a97eb30fcc205
Snoopyh <ismaelxd500@gmail.com>
2022-10-20 21:32:01 -0300

    Curso-React-Cod3r

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Primeiro from './componets/basicos/Primeiro'
+import './index.css'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+   <Primeiro/>
+   <Primeiro/>
+  </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
