354df386c95837330587e8bc576221910012b4bd
Snoopyh <ismaelxd500@gmail.com>
2022-10-25 14:49:21 -0300

    Changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Primeiro from './componets/basicos/Primeiro'
 import './index.css'
+
+
+import ComParametro from './componets/basicos/ComParametro';
+import Primeiro from './componets/basicos/Primeiro'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
    <Primeiro/>
-   <Primeiro/>
+   <ComParametro titulo="Situação do Aluno"
+   aluno="Gustavo Silva"
+   nota={9.3}
+   />
   </>
 );
 
