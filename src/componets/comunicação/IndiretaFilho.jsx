8ceb842c809c276f431f4b32ba47dc77643054f0
Snoopyh <ismaelxd500@gmail.com>
2022-10-29 15:58:12 -0300

    last Changes

--- src/componets/comunicação/IndiretaFilho.jsx
+++ src/componets/comunicação/IndiretaFilho.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
-
 export default function(props) {
+  const cb =props.quandoClicar
+  const gerarIdade = () => Math.floor(Math.random() * (50 - 20) + 20 )
+  const gerarNerd = () => Math.random() > 0.5
   return (
     <div>
-      Filho
+      <div>Filho</div>
+      <button onClick={(_) => cb("João", gerarIdade(), gerarNerd())}>
+        Fornecer Informaçôes
+      </button>
     </div>
   );
 }
--- src/componets/comunicação/IndiretaPai.jsx
+++ src/componets/comunicação/IndiretaPai.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React,{useState} from "react";
 import IndiretaFilho from "./IndiretaFilho";
 
 export default function(props) {
+  const [nome ,setNome] = useState("?");
+  const [idade ,setIdade] = useState(0);
+  const [nerd ,setNerd] = useState(true);
+  
+  function fornecerInfo(nome, idade, nerd ){
+    setNome(nome)
+    setIdade(idade)
+    setNerd(nerd)
+  }
   return (
     <div>
-      Pai
-      <IndiretaFilho></IndiretaFilho>
+      <div>
+        <span>{nome} </span>
+        <span>{idade} </span>
+        <span>{nerd ? "Verdadeiro" : "false"}</span>
+      </div>
+      <IndiretaFilho quandoClicar={fornecerInfo}></IndiretaFilho>
     </div>
   );
 }
